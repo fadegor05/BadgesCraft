fb8db55af68abcc74e3f5ca3a458ec8fa7e443c4
fadegor05 <fad.egor05@gmail.com>
2023-12-06 17:54:46 +0400

    init commit

---
+++ src/main/java/me/fadegor05/badgescraft/BadgesCraft.java
@@ -0,0 +1,20 @@
+package me.fadegor05.badgescraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class BadgesCraft extends JavaPlugin {
+
+    private static final Logger logger = Logger.getLogger("Minecraft");
+
+    private static BadgesCraft plugin;
+
+    public static BadgesCraft getPlugin() { return plugin; }
+
+    @Override
+    public void onEnable() {
+        logger.info("BadgesCraft 1.0 has been enabled!");
+    }
+
+}
