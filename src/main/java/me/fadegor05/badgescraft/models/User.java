9a90b0842ebf8009266cd6badc063bc7f0192a4d
fadegor05 <fad.egor05@gmail.com>
2023-12-19 13:58:43 +0400

    #3 commit
    User class changes
    Getter and setters added in User class

--- src/main/java/me/fadegor05/badgescraft/models/User.java
+++ src/main/java/me/fadegor05/badgescraft/models/User.java
@@ -4,8 +4,39 @@ import java.util.ArrayList;
 
 public class User {
     private String playerName;
-    private ArrayList<Template> templates;
-    private ArrayList<Color> colors;
-    private ArrayList<Badge> badges;
+    private ArrayList<String> templates;
+    private ArrayList<String> colors;
+    private ArrayList<String> badges;
 
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public ArrayList<String> getTemplates() {
+        return templates;
+    }
+
+    public void setTemplates(ArrayList<String> templates) {
+        this.templates = templates;
+    }
+
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(ArrayList<String> colors) {
+        this.colors = colors;
+    }
+
+    public ArrayList<String> getBadges() {
+        return badges;
+    }
+
+    public void setBadges(ArrayList<String> badges) {
+        this.badges = badges;
+    }
 }
--- src/main/java/me/fadegor05/badgescraft/utils/UserStorageUtil.java
+++ src/main/java/me/fadegor05/badgescraft/utils/UserStorageUtil.java
@@ -1,4 +1,5 @@
 package me.fadegor05.badgescraft.utils;
 
 public class UserStorageUtil {
+
 }
